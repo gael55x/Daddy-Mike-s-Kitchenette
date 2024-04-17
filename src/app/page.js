5efc8ac194f9ef05ad44c02654d1f199629ffe4d
gamolong <gaille.amolong1@gmail.com>
2024-04-17 16:06:41 +0800

    initialized design and project structure

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p><Link href="/about">About Us</Link></p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Daddy Mikes Kitchenette
+      </h1>
+      <p><Link href="/meals">Meals</Link></p>
+      <p><Link href="/meals/share">Share Meal</Link></p>
+      <p><Link href="/community">Community</Link></p>
     </main>
   );
 }
